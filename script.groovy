6557c5de8efbf67cb981a18dd3ec14bed9712fd2
idris <yusuffidris97@gmail.com>
2023-11-07 16:59:49 +0100

    building app

--- script.groovy
+++ script.groovy
@@ -5,7 +5,7 @@ def buildImage() {
         sh 'docker build --file=presentation-tier/Dockerfile -t yusidris/web-tier:2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push yusidris/app-tier:2.0'
-        sh 'docker push yusidris/webtier:2.0'
+        sh 'docker push yusidris/web-tier:2.0'
 
     }
 } 
