0a7650c7fabe23aebade363a323808a13e4a3e3b
idris <yusuffidris97@gmail.com>
2023-11-07 14:57:41 +0100

    building app

---
+++ script.groovy
@@ -0,0 +1,13 @@
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker_psw', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build --file=application-tier/Dockerfile -t yusidris/app-tier:2.0 .'
+        sh 'docker build --file=presentation-tier/Dockerfile -t yusidris/web-tier:2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push yusidris/app-tier:1.0'
+        sh 'docker push yusidris/webtier:1.0'
+
+    }
+} 
+
+return this
